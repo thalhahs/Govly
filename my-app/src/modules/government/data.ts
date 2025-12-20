3ae9a74c1c64ddc1c36bb5eeaebba40c331470c5
thalhahs <thalhah@gmail.com>
2025-12-20 21:14:35 +0700

    feat: edit slug

--- my-app/src/modules/government/data.ts
+++ my-app/src/modules/government/data.ts
@@ -13,7 +13,7 @@ export const dataGovernments: Governments = [
   {
     id: 1,
     name: "Department of Agriculture",
-    slug: "Agriculture",
+    slug: "department-of-agriculture",
     description:
       "The Department of Agriculture is responsible for the production and distribution of food and agriculture.",
     createdAt: new Date(),
@@ -22,7 +22,7 @@ export const dataGovernments: Governments = [
   {
     id: 2,
     name: "Department of Defense",
-    slug: "Defense",
+    slug: "department-of-defense",
     description: "The Department of Defense is responsible for the defense of the country.",
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -30,9 +30,11 @@ export const dataGovernments: Governments = [
   {
     id: 3,
     name: "Department of Education",
-    slug: "Education",
+    slug: "department-of-education",
     description: "The Department of Education is responsible for the education of the country.",
     createdAt: new Date(),
     updatedAt: new Date(),
   },
 ];
+
+

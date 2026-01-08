ca995bcf243cf37d4a5e3c4d372b8f86427ce1ad
thalhahs <thalhah@gmail.com>
2026-01-08 17:53:20 +0700

    feat: setup postgres database connection example, install dependencies

---
+++ example.ts
@@ -0,0 +1,15 @@
+import * as pg from "pg";
+
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+await client.connect();
+
+const result = await client.query("SELECT * FROM governments");
+
+const governments = result.rows;
+
+console.log({ governments });
+
+await client.end();
